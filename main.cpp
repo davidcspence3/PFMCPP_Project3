42eca447f1d00406e07cde0fb8dbdb8469c6c888
davidcspence3 <davidcspence3@gmail.com>
2020-08-02 00:23:15 +0000

    finished part 1a

--- main.cpp
+++ main.cpp
@@ -35,51 +35,51 @@ Sub Object 1: Steering Wheel
     Name 4 things you'll find on the:    Steering Wheel
         1) paddle shifters
         2) 'cruise control' controls
-        3) 
-        4) 
+        3) 'turn signal' controls
+        4) windshield wiper controls
     Name 2 things you can do with the:   Steering Wheel
         1) adjust cruise control settings.
-        2)
+        2) turn on windshield wipers
         
 Sub Object 2: Instrument Cluster
     Name 4 things you'll find on the:   Instrument Cluster
-        1)
-        2)
-        3)
-        4)
+        1) speedometer
+        2) odometer
+        3) check engine light
+        4) gasoline gauge
     Name 3 things you can do with the:   Instrument Cluster
-        1)
-        2)
-        3)
+        1) know how fast you are going
+        2) know how far you have gone
+        3) check how much gas you have left
     
 Sub Object 3: Environment Controls
     Name 3 things you'll find on the:    Environment Controls
-        1)
-        2)
-        3)
+        1) A/C on/off
+        2) Fan level
+        3) Defrost controls
     Name 3 things you can do with the:   Environment Controls
-        1)
-        2)
-        3)
+        1) turn on the air conditioning
+        2) direct air to your feet
+        3) turn on the defrost
 
 Sub Object 4: Infotainment System
     Name 3 things you'll find on the:    Infotainment System
-        1)
-        2)
-        3)
+        1) clock
+        2) cd player 
+        3) radio tuner
     Name 3 things you can do with the:   Infotainment System
-        1)
-        2)
-        3)
+        1) change radio station
+        2) insert a cd
+        3) change the time
 
 Sub Object 5: Seat 
     Name 3 things you'll find on the:    Seat
-        1)
-        2)
-        3)
+        1) seatbelt
+        2) recline switch
+        3) forward/backwards mover switch
     Name 2 things you can do with the:   Seat
-        1)
-        2)
+        1) buckle the seatbelt
+        2) recline the seat
 */
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
